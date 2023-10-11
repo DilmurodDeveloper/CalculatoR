6d1718cc3f575c2ac9653e3630c012cd3fdfff1b
Dilmurod <madirimovdilmurod2@gmail.com>
2023-10-11 10:13:13 +0500

    first commit

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+System.Console.Write("Birinchi sonni kiriting: ");
+int firstnumber = Convert.ToInt32(Console.ReadLine());
+
+System.Console.Write("Ikkinchi sonni kiriting: ");
+int secondnumber = Convert.ToInt32(Console.ReadLine());
+
+System.Console.WriteLine($"Adding {firstnumber + secondnumber}");
+System.Console.WriteLine($"Substracting {firstnumber - secondnumber}");
+System.Console.WriteLine($"Substracting {firstnumber / secondnumber}");
+System.Console.WriteLine($"Multiplying {firstnumber * secondnumber}");
+System.Console.WriteLine($"Qoldiq {firstnumber % secondnumber}");
